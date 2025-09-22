92e5605ca4731d32e9ee8f1fc72e3352a3ca1060
ozkan-kaya <ozkankaya4734@gmail.com>
2025-09-22 17:30:09 +0300

    a new training topic has been completed

--- Temel Egitimler/JS Pratikleri/Form/3- FormValidation/js.js
+++ Temel Egitimler/JS Pratikleri/Form/3- FormValidation/js.js
@@ -1,11 +1,24 @@
-const username = '111ozkannn';
-const pattern = /^[a-z]{6,10}$/;
+const form = document.querySelector('form');
+const message = document.querySelector('.message');
+const usernamePattern = /^[a-z]{6,10}$/;
 
-let sonuc = pattern.test(username);
-// console.log(sonuc);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const username = form.username.value;
 
-if (sonuc) console.log('basarili');
-else console.log('basarisiz');
+    if (usernamePattern.test(username)) message.textContent = 'basarili';
 
-sonuc = username.search(pattern);
-console.log(sonuc);
+    else message.textContent = 'Tum harfler kucuk harf ve 6-10 karakter arasinda giriniz.';
+})
+
+form.username.addEventListener('keyup', (e) => {
+    // console.log(e.target.value, form.username.value);
+    if (usernamePattern.test(e.target.value)) {
+        // console.log('basarili');
+        form.username.setAttribute('class', 'success');
+    }
+    else {
+        // console.log('basarisiz');
+        form.username.setAttribute('class', 'error');
+    }
+})
