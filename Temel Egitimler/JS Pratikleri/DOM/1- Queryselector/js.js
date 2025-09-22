dab3e397d4cfe2f9a32cbf95ccc82d1ad638a8f6
ozkan-kaya <ozkankaya4734@gmail.com>
2025-09-22 10:13:58 +0300

    new training topic has been completed

---
+++ Temel Egitimler/JS Pratikleri/DOM/1- Queryselector/js.js
@@ -0,0 +1,11 @@
+// const hata = document.querySelector('p');
+// const hata = document.querySelector('.error');
+// const hata = document.querySelector('div.error');
+// console.log(hata);
+
+const hatalar = document.querySelectorAll('p');
+console.log(hatalar);
+
+hatalar.forEach(hata => {
+    console.log(hata);
+});
