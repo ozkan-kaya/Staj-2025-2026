76c77af1ea1e1669859ef52e87db2492e4e2d676
ozkan-kaya <ozkankaya4734@gmail.com>
2025-09-30 17:30:07 +0300

    a new training topic has been complated

---
+++ Temel Egitimler/TypeScript Pratikleri/1- Kurulum/app.js
@@ -0,0 +1,15 @@
+console.log('Typscripte giris');
+console.log('ozko');
+var id = 99;
+var firstName = 'alice';
+var isUpdated = true;
+function display(id, name) {
+    console.log("id: " + id + ", name: " + name);
+}
+display(id, firstName);
+// DATA TYPES
+var first = 123;
+var second = 0x37CF; //hexadecimal (onluk sistem = 14287)
+var third = 255; //octal (onluk sistem = 255)
+var fourth = 57; //binary (onluk sistem = 57)
+console.log(first, second, third, fourth);
